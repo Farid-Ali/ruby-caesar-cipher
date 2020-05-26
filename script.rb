73e86c932e5c4904e74674690ff7a863003129a0
Farid Ali <faridalicode@gmail.com>
2020-05-26 23:31:11 +0530

    Bug fixes and improvement

--- script.rb
+++ script.rb
@@ -6,7 +6,6 @@ puts "Input the string which you want to be convert:"
 #Store the inputed string to a variable
 
 user_inputed_string = gets.chomp
-p user_inputed_string
 
 #Ask user for a shift factor
 
@@ -17,7 +16,6 @@ puts "Input the Shift factor:"
 #Store the shift factor to a variable
 
 user_inputed_shift_factor = gets.chomp.to_i
-p user_inputed_shift_factor
 
 ## Caeser_cipher method
 def caeser_cipher(user_inputed_string, user_inputed_shift_factor)
@@ -27,7 +25,6 @@ def caeser_cipher(user_inputed_string, user_inputed_shift_factor)
   #Store each character code to an array
 
   string_character_codes = user_inputed_string.codepoints.to_a
-  p string_character_codes
 
   #Character code set
 
@@ -39,7 +36,7 @@ def caeser_cipher(user_inputed_string, user_inputed_shift_factor)
     end
   end
 
-  #Mod
+  #Mod(Chaque 'modular arithmetic' to learn about this in details)
 
   def mod(n, m)
     (n % m + m) % m
@@ -56,23 +53,20 @@ def caeser_cipher(user_inputed_string, user_inputed_shift_factor)
       code + 0
     end
   end
-  p character_codes_with_shift_factor
 
   #Convert each modified character code to character
   #Store each modified character to an array
 
   modified_characters = character_codes_with_shift_factor.map { |modified_code| modified_code.chr }
-  p modified_characters
 
   #Join each modified character to a string
   #Store the modified string to a variable
   #Return the variable which holds the modified string
 
   new_string = modified_characters.join
-  p new_string
 
 end
 
 #Show the result to the User
-
-puts caeser_cipher(user_inputed_string, user_inputed_shift_factor)
+puts "Your Inputed String: #{user_inputed_string} And Your Inputed Shift: #{user_inputed_shift_factor}"
+puts "Result: #{caeser_cipher(user_inputed_string, user_inputed_shift_factor)}"
